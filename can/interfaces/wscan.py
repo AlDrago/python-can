88dee6f0eb56472b5e44783460599a254704cfd6
Alan Dragomirecký <alan@dragomirecky.com>
2016-11-19 19:31:38 +0100

    Better threading&asyncio cooperation

--- can/interfaces/wscan.py
+++ can/interfaces/wscan.py
@@ -3,6 +3,7 @@ import websockets
 import asyncio
 import time
 import json
+import threading
 from can.can2cane.CaneMessage import CaneMessage
 from can.bus import BusABC
 from can.message import Message
@@ -15,18 +16,41 @@ class WSBus(BusABC):
     """ Adds support for Cane protocol (CAN over websocket) """
 
     def __init__(self, channel, *args, **kwargs):
+        self.url = kwargs['url']
+        self.socket = None
+        # start the loop
         self.loop = asyncio.new_event_loop()
-        self.socket = self.loop.run_until_complete(websockets.connect(kwargs['url'], loop=self.loop, timeout=0))
+        self.thread = threading.Thread(target=self.loop.run_forever)
+        self.thread.start()
+        # connect
+        asyncio.run_coroutine_threadsafe(self.ensure_connected(), self.loop).result()
+
+    async def ensure_connected(self):
+        while self.socket is None or self.socket.open == False:
+            try:
+                self.socket = await websockets.connect(self.url, loop=self.loop, timeout=0)
+            except:
+                print('Could not connect, reconnectig ...')
+                time.sleep(1)
+
+    async def recv_io(self):
+        await self.ensure_connected()
+        return await self.socket.recv()
+
+    async def send_io(self, data):
+        await self.ensure_connected()
+        return await self.socket.send(data)
 
     def recv(self, timeout=None):
-        data = self.loop.run_until_complete(self.socket.recv())
+        future = asyncio.run_coroutine_threadsafe(self.recv_io(), self.loop)
+        data = future.result(timeout)
         message = self.deserialize_message(data)
         return message
 
     def send(self, message):
         message.timestamp = time.time()
         data = self.serialize_message(message)
-        self.loop.run_until_complete(self.socket.send(data))
+        return asyncio.run_coroutine_threadsafe(self.send_io(data), self.loop).result()
 
     def deserialize_message(self, data):
         msg = json.loads(data)
@@ -37,5 +61,6 @@ class WSBus(BusABC):
         return json.dumps(msg)
 
     def shutdown(self):
-        self.loop.run_until_complete(self.socket.close())
-        self.loop.close()
+        future = asyncio.run_coroutine_threadsafe(self.socket.close(), self.loop)
+        future.result()
+        self.loop.call_soon_threadsafe(self.loop.stop)
